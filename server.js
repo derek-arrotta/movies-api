53d3d11d480ba9d3f2aee4caf6ca4a62db9a3cc6
Derek Arrotta <derek.arrotta@gmail.com>
2021-01-10 18:17:29 -0500

    add validcountry list

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const morgan = require('morgan')
 
+console.log(process.env.API_TOKEN)
+
 const app = express()
 
 app.use(morgan('dev'))
 
 const validGenres = [`Animation`, `Drama`, `Romantic`, `Comedy`, `Spy`, `Crime`, `Thriller`, `Adventure`, `Documentary`, `Horror`, `Action`, `Western`, `History`, `Biography`, `Musical`, `Fantasy`, `War`, `Grotesque`]
+const validCountry = [`United States`, `Italy`, `Germany`, `Israel`, `Great Britain`, `France`, `Hungary`, `China`, `Canada`, `Spain`, `Japan`]
 
 function handleGetTypes(req, res) {
   res.json(validGenres)
+  res.json(validCountry)
 }
   
-app.get('/types', handleGetTypes)
+app.get('/movies', handleGetTypes)
 
 const PORT = 8000
 
